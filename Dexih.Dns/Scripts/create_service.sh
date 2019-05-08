c00422e8053b52c95ce0b4fb4d85f6be59617564
garyholland <gholland@dataexpertsgroup.com>
2019-05-08 14:53:31 +1000

    fixed scripts

--- Dexih.Dns/Scripts/create_service.sh
+++ Dexih.Dns/Scripts/create_service.sh
@@ -1,4 +1,7 @@
+mkdir /hoem/ubuntu/app
+dotnet publish -r Release -o /home/ubuntu/app
+
 sudo cp dexih.dns.service /etc/systemd/system
 sudo systemctl daemon-reload
-sudo systemctl enable dexih-dns.service
-sudo systemctl start dexih-dns.service
+sudo systemctl enable dexih.dns.service
+sudo systemctl start dexih.dns.service
