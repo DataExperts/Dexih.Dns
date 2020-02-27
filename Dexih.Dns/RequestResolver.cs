f6843163b9f55811efb4137b38fe8298258856ec
garyholland <gholland@dataexpertsgroup.com>
2020-02-27 16:27:09 +1100

    minor

--- Dexih.Dns/RequestResolver.cs
+++ Dexih.Dns/RequestResolver.cs
@@ -8,9 +8,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
-using System.Text;
 using System.Threading.Tasks;
-using Microsoft.Extensions.Caching.Memory;
 using Microsoft.Extensions.Logging;
 
 namespace Dexih.Dns
